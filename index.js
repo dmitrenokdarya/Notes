5bbdec248e4b48868a6f404bbde74c90963fafb7
Darya Dmitrenok <dmitrenokdarya@gmail.com>
2024-11-25 19:53:05 +0300

    hnml_css_js_for_notes_next_part

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-const MOCK_TASKS = [
-    { id: 1, title: 'Изучить паттерн MVC', description: 'Aрхитектурный паттерн, который разделяет приложение на три основные части', color: blue, isSelected: true },
-    { id: 2, title: 'Подготовить моковые данные', description: 'Каждая задача будет представлена в виде объекта с пятью свойствами: id, title, description, color, isSelected', color: pink, isSelected: true },
-]
-
-
 const model = {
     tasks: [],
 
     addTask(title, description, color) {
-        const isSelected = false
-        const id = Math.random()
+        const isSelected = false;
+        const id = Math.random();
+        console.log(description);
+        const newTask = { title, description, color, isSelected, id };
 
-        const newTask = { title, description, isSelected, color, id }
+        this.tasks.unshift(newTask);
 
-        this.tasks.push(newTask)
-
-        view.renderTasks(model.tasks) // Обновляем представление
+        view.renderTasks(model.tasks); // Обновляем представление 
     },
 }
 
@@ -28,33 +22,44 @@ const view = {
         const input = document.querySelector('.input')
         const textarea = document.querySelector('.textarea')
 
-        // Добавляем обработчик события на форму
+        // Добавляем обработчик события на форму 
         form.addEventListener('submit', function (event) {
-            event.preventDefault() // Предотвращаем стандартное поведение формы
+            event.preventDefault() // Предотвращаем стандартное поведение формы 
             const title = document.querySelector('.input').value
             const description = document.querySelector('.textarea').value
-            controller.addTask(title, description) // Вызываем метод addTask контроллера
+            const color = document.getElementsByName('color:checked').value //??? 
 
-            input.value = '' // Очищаем поле ввода
+            console.log(color)
+            console.log(description)
+            controller.addTask(title, description, color) // Вызываем метод addTask контроллера 
+
+            input.value = '' // Очищаем поле ввода 
             textarea.value = ''
         })
     },
 
     renderTasks(tasks) {
         const list = document.querySelector('.list')
+        let count = document.querySelector('.number_of_notes')
+        if (tasks.length === 0) {
+            const textZeroNotes = document.querySelector('.textZeroNotes')
+            textZeroNotes.innerHTML = 'У вас ещё нет ни одной заметки.<br>Заполните поля выше и создайте свою первую заметку!'
 
-        let tasksHTML = ''
-        tasks.forEach((task) => {
-            tasksHTML += `
-            <li id="${task.id}" class="${task.isSelected ? 'selected' : ''}">
-                <p class="task-title">${task.title}</p>
-                <p class="task-description">${task.description}</p>
-                <button class="selected-button" type="button">???</button>
-                <button class="delete-button" type="button"><img src="assets/img/trash.png" alt="Delete"></button>
-            </li>
-            `
-        })
-        list.innerHTML = tasksHTML
+        } else {
+            let tasksHTML = ''
+            tasks.forEach((task) => {
+                tasksHTML += ` 
+                    <li id="${task.id}" class="${task.isSelected ? 'selected' : ''}"> 
+                    <p class="task-title">${task.title}</p> 
+                    <p class="task-description">${task.description}</p> 
+                    <button class="selected-button" type="button">???</button> 
+                    <button class="delete-button" type="button"><img src="assets/img/trash.png" alt="Delete"></button> 
+                    </li> 
+                    `
+                list.innerHTML = tasksHTML
+            })
+        }
+        count.innerHTML = tasks.length
     }
 }
 
@@ -62,8 +67,8 @@ const controller = {
     addTask(title, description) {
         if (title.trim() !== '' && title.length <= 50 && description.trim() !== '') {
             model.addTask(title)
-        } else if (title.length > 50){
-            //Картинка, но пока не знаю, куда
+        } else if (title.length > 50) {
+            //Картинка, но пока не знаю, куда 
         }
     },
 }
@@ -75,6 +80,3 @@ function init() {
 }
 
 init()
-
-
-
